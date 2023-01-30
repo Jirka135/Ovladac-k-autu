68c6ee2cb78579b00b9a0950f004440f35886a2f
Jirka135 <54354077+Jirka135@users.noreply.github.com>
2023-01-30 16:30:40 +0100

    Version 0.0.2

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <WiFi.h>
+#include <M5StickCPlus.h>
+
+const char* ssid = "jirka";
+const char* password = "jirka123";
+
+const char* serverAddress = "10.0.0.50"; // Replace with the IP address of the server ESP32
+const int serverPort = 80;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  WiFiClient client;
+  if (!client.connect(serverAddress, serverPort)) {
+    Serial.println("Could not connect to server");
+    delay(1000);
+    return;
+  }
+
+  String message = "Hello from the client ESP32";
+  client.print(message);
+  Serial.println("Message sent to server");
+
+  String response = client.readStringUntil('\r');
+  Serial.println("Response from server: " + response);
+
+  client.stop();
+  delay(3000);
+}
